d8c62ca4e19f0916cf8b27aee68353c47623494f
Ayush Thakur <ayt12321232@gmail.com>
2025-04-03 13:07:27 +0530

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Page',
+      title: 'Login Pagee',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
